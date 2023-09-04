7a4ce1359a0e7c1b52418d1e9c1e6814360e77ba
Vaishnavi Panchal <91953299+Vaishnaviii25@users.noreply.github.com>
2023-09-04 23:49:35 +0530

    Time: 390 ms (67.40%), Space: 10.4 MB (11.42%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -7,24 +7,17 @@ class Solution {
     nums.insert(nums.end(), 1);
 
     // dp[i][j] := maxCoins(nums[i..j])
-    dp.resize(n + 2, vector<int>(n + 2));
-    return maxCoins(nums, 1, n);
-  }
+    vector<vector<int>> dp(n + 2, vector<int>(n + 2));
 
- private:
-  vector<vector<int>> dp;
+    for (int d = 0; d < n; ++d)
+      for (int i = 1; i + d <= n; ++i) {
+        const int j = i + d;
+        for (int k = i; k <= j; ++k)
+          dp[i][j] = max(dp[i][j],                      //
+                         dp[i][k - 1] + dp[k + 1][j] +  //
+                             nums[i - 1] * nums[k] * nums[j + 1]);
+      }
 
-  int maxCoins(vector<int>& nums, int i, int j) {
-    if (i > j)
-      return 0;
-    if (dp[i][j] > 0)
-      return dp[i][j];
-
-    for (int k = i; k <= j; ++k)
-      dp[i][j] = max(dp[i][j],                                              //
-                     maxCoins(nums, i, k - 1) + maxCoins(nums, k + 1, j) +  //
-                         nums[i - 1] * nums[k] * nums[j + 1]);
-
-    return dp[i][j];
+    return dp[1][n];
   }
 };
